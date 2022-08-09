67380c041fecb73da87ce25858e2a54e2491f523
rjoc <rjoc@protonmail.com>
2022-08-09 03:33:27 +0100

    Group Login Block

--- frontend/src/pages/Groups.js
+++ frontend/src/pages/Groups.js
@@ -4,12 +4,19 @@ import { Footer } from "../components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import GroupBody from "../components/Misc/GroupBody";
 function Groups() {
+  const navigateLog = useNavigate();
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+    if (!user) {
+      navigateLog("/login");
+    } else {
+      navigateLog("/groups");
+    }
+  }, [navigateLog]);
   return (
     <div className="Inspire">
       <NavBar />
-
       <GroupBody />
-
       <Footer />
     </div>
   );
