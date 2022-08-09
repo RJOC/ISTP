cff5b2b6371bb6465ef4f67e21887896a8d2352b
rjoc <rjoc@protonmail.com>
2022-08-09 03:38:38 +0100

    Group login block

--- frontend/src/pages/Groups.js
+++ frontend/src/pages/Groups.js
@@ -3,6 +3,9 @@ import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import GroupBody from "../components/Misc/GroupBody";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 function Groups() {
   const navigateLog = useNavigate();
   useEffect(() => {
@@ -13,6 +16,7 @@ function Groups() {
       navigateLog("/groups");
     }
   }, [navigateLog]);
+
   return (
     <div className="Inspire">
       <NavBar />
