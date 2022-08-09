79f8e080e541e2f61d2183ac4cbe6d551f71e723
rjoc <rjoc@protonmail.com>
2022-08-09 02:18:46 +0100

    space

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use("/api/message", messageRoutes);
 
 //HOSTING code
 const __dirname1 = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
   //establishing path from current working directory to the build folder of front end
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
