a2a5ee081c7f0cdab6f8b404f767a84987f3fe4c
rjoc <rjoc@protonmail.com>
2022-08-10 02:55:08 +0100

    Photo update

--- frontend/src/components/DiscoverContent.js
+++ frontend/src/components/DiscoverContent.js
@@ -8,55 +8,71 @@ const DiscoverContent = () => {
   const dataList = [
     {
       id: "1",
-      location: "Edinburgh Castle ",
+      location: "Edinburgh Castle",
       summary:
-        "The stone towers and walls of Edinburgh Castle have dominated the Edinburgh skyline since the 13th century.",
-      longLine: "Very short, can be any description",
-      image:
-        "https://www.planetware.com/photos-large/SCO/scotland-edinburgh-castle-2.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Edinburgh_Castle_from_the_south_east.JPG/1600px-Edinburgh_Castle_from_the_south_east.JPG?20130906211529",
+      image: "",
     },
     {
       id: "2",
-      location: "Loch Lomond",
-      summary:
-        "Idyllic Loch Lomond, just a short drive northwest of Glasgow, is Britain's largest lake.",
-      longLine: "Billy Bob Bob Bob Bob likes Markiplier gameplay videos",
+      location: "Camera Obscura & World of Illusions",
+      summary: "",
       image:
         "https://www.visitbritain.com/sites/default/files/styles/consumer_vertical_hero__1920x1080/public/paragraphs_bundles/hero/scotland_0.jpg?itok=moGcC_81",
     },
     {
       id: "3",
-      location: " Loch Ness ",
-      summary:
-        "Think of Loch Ness and you'll probably picture the mythical monster that, according to legend, has made this 23-mile-long loch home for countless centuries",
-      longLine: "Wow, this is very descriptive! I wonder how long it is",
+      location: "Authur's Seat",
+      summary: "",
       image:
         "https://deih43ym53wif.cloudfront.net/cairngorms-national-park-scotland-shutterstock_726100102_4ab443fcc9.jpeg",
     },
     {
       id: "4",
-      location: "Edinburgh Castle ",
-      summary:
-        "The stone towers and walls of Edinburgh Castle have dominated the Edinburgh skyline since the 13th century.The stone towers and walls of Edinburgh Castle have dominated the Edinburgh skyline since the 13th century.The stone towers and walls of Edinburgh Castle have dominated the Edinburgh skyline since the 13th century.The stone towers and walls of Edinburgh Castle have dominated the Edinburgh skyline since the 13th century.The stone towers and walls of Edinburgh Castle have dominated the Edinburgh skyline since the 13th century.The stone towers and walls of Edinburgh Castle have dominated the Edinburgh skyline since the 13th century.",
-      longLine: "Very short, can be any description",
+      location: "The Kelpies",
+      summary: "",
       image:
         "https://i.guim.co.uk/img/media/7f434d0e05ac6d8c3ca8fde35f4a0972e03c7cb6/0_181_5482_3291/master/5482.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=56a71cf7af13b5d0b5b6faeedf7579e8",
     },
     {
       id: "5",
-      location: "Loch Lomond",
-      summary:
-        "Idyllic Loch Lomond, just a short drive northwest of Glasgow, is Britain's largest lake.",
-      longLine: "Billy Bob Bob Bob Bob likes Markiplier gameplay videos",
+      location: "Calton Hill",
+      summary: "",
       image:
         "https://www.planetware.com/photos-large/SCO/scotland-edinburgh-castle-2.jpg",
     },
     {
       id: "6",
-      location: " Loch Ness ",
-      summary:
-        "Think of Loch Ness and you'll probably picture the mythical monster that, according to legend, has made this 23-mile-long loch home for countless centuries",
-      longLine: "Wow, this is very descriptive! I wonder how long it is",
+      location: "Edinburgh Zoo",
+      summary: "",
+      image:
+        "https://cdn.pixabay.com/photo/2016/10/22/17/46/mountains-1761292_1280.jpg",
+    },
+    {
+      id: "7",
+      location: "Loch Ness",
+      summary: "",
+      image:
+        "https://cdn.pixabay.com/photo/2016/10/22/17/46/mountains-1761292_1280.jpg",
+    },
+    {
+      id: "8",
+      location: "Royal Mile",
+      summary: "",
+      image:
+        "https://cdn.pixabay.com/photo/2016/10/22/17/46/mountains-1761292_1280.jpg",
+    },
+    {
+      id: "9",
+      location: "Dynamic Earth",
+      summary: "",
+      image:
+        "https://cdn.pixabay.com/photo/2016/10/22/17/46/mountains-1761292_1280.jpg",
+    },
+    {
+      id: "10",
+      location: "Scott Monument",
+      summary: "",
       image:
         "https://cdn.pixabay.com/photo/2016/10/22/17/46/mountains-1761292_1280.jpg",
     },
@@ -79,7 +95,7 @@ const DiscoverContent = () => {
           color={"#8FBBDA!important"}
           paddingTop="20px"
         >
-          Top-Rated Tourist Attractions in Scotland
+          The Editor's Choice of Attractions in Scotland
         </Text>
         {/* CARD START */}
         <Container maxW="80rem" centerContent>
