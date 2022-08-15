294b2f2a46ae76ed297050eace21069a93b06fdd
rjoc <rjoc@protonmail.com>
2022-08-16 00:48:40 +0100

    Remove default profile pic

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, required:true, unique:true},
-    password:{type:String, required:true},
-    picture:{type:String,default:"https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-2.jpg"},
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    picture: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-    timestamps:true,
-});
-
-userSchema.methods.matchPassword= async function (enteredPassword){
-    return await bcrypt.compare(enteredPassword,this.password);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre('save',async function (next) {
-    if (!this.isModified){
-        next()
-    }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password,salt);
-})
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
